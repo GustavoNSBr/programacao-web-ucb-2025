489f8763a4d3ac0a1bc2c795c2b33d2cb4e9074f
GustavoNSBr <gustavonunessilvabr@gmail.com>
2025-03-06 09:21:46 -0300

    testando

---
+++ variaveis_tipos/variaveis_tipos.js
@@ -0,0 +1,18 @@
+// console.log("Hello, World");
+
+const n = 3;
+
+var j = 6;
+var j = 7;
+
+let i = 8;
+i = 9;
+
+let nome = "Joao";
+let telefone = '1234';
+
+console.log(`Meu nome é ${nome}. Telefone: ${telefone}`);
+
+let b = true;
+
+console.log(`b= ${b}`);
