4a475ff522fa42ae4be1f07c64dc8db8e5e6324e
GustavoNSBr <gustavonunessilvabr@gmail.com>
2025-03-06 09:32:53 -0300

    atividade + atividade corrigida

--- atividade1/variaveisOperadores.js
+++ atividade1/variaveisOperadores.js
@@ -1,13 +1,29 @@
 
-let num_A = 10;
-let num_B = 5;
+// let numA = 10;
+// let numB = 5;
 
-let soma = num_A + num_B;
-let subtracao = num_A - num_B;
-let multiplicacao = num_A * num_B;
-let divisao = num_A / num_B;
+// let soma = numA + numB;
+// let subtracao = numA - numB;
+// let multiplicacao = numA * numB;
+// let divisao = numA / numB;
 
-console.log(`Resultado da soma entre ${num_A} e ${num_B} é ${soma}`);
-console.log(`Resultado da subtração entre ${num_A} e ${num_B} é ${subtracao}`);
-console.log(`Resultado da multiplicação entre ${num_A} e ${num_B} é ${multiplicacao}`);
-console.log(`Resultado da divisão entre ${num_A} e ${num_B} é ${divisao}`);
+// console.log(`Resultado da soma entre ${numA} e ${numB} é ${soma}`);
+// console.log(`Resultado da subtração entre ${numA} e ${numB} é ${subtracao}`);
+// console.log(`Resultado da multiplicação entre ${numA} e ${numB} é ${multiplicacao}`);
+// console.log(`Resultado da divisão entre ${numA} e ${numB} é ${divisao}`);
+
+
+
+let numA = 2.8;
+let numB = 1.4;
+let numC; // undefined
+
+numC = numA + numB;
+console.log(`Resultado da adição entre ${numA} e ${numB} é ${numC}`);
+
+numC = numA - numB;
+console.log(`Resultado da subtração entre ${numA} e ${numB} é ${numC}`);
+numC = numA * numB;
+console.log(`Resultado da multiplicação entre ${numA} e ${numB} é ${numC}`);
+numC = numA / numB;
+console.log(`Resultado da divisão entre ${numA} e ${numB} é ${numC}`);
