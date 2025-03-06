aaaba0abc5d41ef3cd8d362cdff4de085ae98f5b
GustavoNSBr <gustavonunessilvabr@gmail.com>
2025-03-06 09:42:00 -0300

    teste

--- atividade1/variaveisOperadores.js
+++ atividade1/variaveisOperadores.js
@@ -23,7 +23,9 @@ console.log(`Resultado da adição entre ${numA} e ${numB} é ${numC}`);
 
 numC = numA - numB;
 console.log(`Resultado da subtração entre ${numA} e ${numB} é ${numC}`);
+
 numC = numA * numB;
 console.log(`Resultado da multiplicação entre ${numA} e ${numB} é ${numC}`);
+
 numC = numA / numB;
 console.log(`Resultado da divisão entre ${numA} e ${numB} é ${numC}`);
